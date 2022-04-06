0e5c50911dd55e5c1e47b241c19c8fc56d9fd4fb
GiamBal <gimmy_1996@hotmail.it>
2022-04-06 22:02:57 +0200

    changed color picker, 1 state to handle all mashes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,40 +1,60 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense, useState } from "react";
+import { proxy, useSnapshot } from "valtio";
+import { HexColorPicker } from "react-colorful";
 import ShoeModel from "./ShoeModel";
-import Colors from "./Colors";
 
 function Main() {
-  const [lacesColor, setLacesColor] = useState("");
-  const [mainColor, setMainColor] = useState("");
-  const [stripesColor, setStripesColor] = useState("");
-  const [soleColor, setSoleColor] = useState("");
+  const [hovered, setHovered] = useState(null);
+
+  const state = proxy({
+    current: hovered,
+    items: {
+      laces: "#ff3",
+      mesh: "#fff",
+      caps: "#3f3",
+      inner: "#3f3",
+      sole: "#3f3",
+      stripes: "#3f3",
+      band: "#3f3",
+      patch: "#3f3",
+    },
+  });
+
+  const [color, setcolor] = useState("#ffffff");
+
+  const snap = useSnapshot(state);
+
   return (
     <div className="bodyContainer">
       <div className="innerContainer">
         <div className="canvasContainer">
+          <HexColorPicker
+            color={snap.items[snap.current]}
+            onChange={(color) => (state.items[snap.current] = color)}
+          />
+          <h1>{snap.current}</h1>
+          <h1>{snap.items[snap.current]}</h1>
           <Canvas>
             <Suspense fallback={null}>
               <ambientLight />
+              <spotLight
+                intensity={0.5}
+                angle={0.1}
+                penumbra={1}
+                position={[10, 15, 10]}
+                castShadow
+              />
               <ShoeModel
                 scale={3}
-                lacesColor={lacesColor}
-                mainColor={mainColor}
-                stripesColor={stripesColor}
-                soleColor={soleColor}
+                hovered={hovered}
+                setHovered={setHovered}
+                state={state}
+                snap={snap}
               />
             </Suspense>
           </Canvas>
         </div>
-        <Colors
-          setLacesColor={setLacesColor}
-          setMainColor={setMainColor}
-          setStripesColor={setStripesColor}
-          setSoleColor={setSoleColor}
-          lacesColor={lacesColor}
-          mainColor={mainColor}
-          stripesColor={stripesColor}
-          soleColor={soleColor}
-        />
       </div>
     </div>
   );
--- src/components/ShoeModel.js
+++ src/components/ShoeModel.js
@@ -1,35 +1,63 @@
 import { useRef } from "react";
 import { useGLTF, OrbitControls } from "@react-three/drei";
 
-export default function Model({ ...props }) {
+export default function Model({ scale, hovered, setHovered, state, snap }) {
   const group = useRef();
   const { nodes, materials } = useGLTF("/shoe.gltf");
+  console.log(snap);
   return (
-    <group ref={group} {...props} dispose={null} scale={props.scale}>
+    <group
+      ref={group}
+      dispose={null}
+      scale={scale}
+      onPointerDown={(e) => {
+        e.stopPropagation();
+        setHovered(e.object.material.name);
+      }}
+      onPointerMissed={(e) => {
+        setHovered(null);
+      }}
+    >
       <mesh
         geometry={nodes.shoe.geometry}
         material={materials.laces}
-        material-color={props.lacesColor}
+        material-color={snap.items.laces}
       />
       <mesh
         geometry={nodes.shoe_1.geometry}
         material={materials.mesh}
-        material-color={props.mainColor}
+        material-color={snap.items.mesh}
       />
-      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} />
-      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} />
+      <mesh
+        geometry={nodes.shoe_2.geometry}
+        material={materials.caps}
+        material-color={snap.items.caps}
+      />
+      <mesh
+        geometry={nodes.shoe_3.geometry}
+        material={materials.inner}
+        material-color={snap.items.inner}
+      />
       <mesh
         geometry={nodes.shoe_4.geometry}
         material={materials.sole}
-        material-color={props.soleColor}
+        material-color={snap.items.sole}
       />
       <mesh
         geometry={nodes.shoe_5.geometry}
         material={materials.stripes}
-        material-color={props.stripesColor}
+        material-color={snap.items.stripes}
       />
-      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} />
-      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} />
+      <mesh
+        geometry={nodes.shoe_6.geometry}
+        material={materials.band}
+        material-color={snap.items.band}
+      />
+      <mesh
+        geometry={nodes.shoe_7.geometry}
+        material={materials.patch}
+        material-color={snap.items.patch}
+      />
       <OrbitControls />
     </group>
   );
